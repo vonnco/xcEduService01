e35251dad7c8e3bdf05b5d40666638f22e9a8d1b
vonco <vonco@itheima.com>
2019-04-30 16:39:24 +0800

    day01:项目搭建，接口测试

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/CmsPageService.java
@@ -0,0 +1,46 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CmsPageService {
+    @Autowired
+    private CmsPageRepository cmsPageRepository;
+
+    /**
+     * 页面列表分页查询
+     * @param page 当前页码
+     * @param size 页面显示个数
+     * @param queryPageRequest 查询条件
+     * @return 页面列表
+     */
+    public QueryResponseResult findList(int page,int size, QueryPageRequest queryPageRequest){
+        if (page <= 0){
+            page = 1;
+        }
+        page = page - 1;//为了适应mongodb的接口将页码减1
+        if (size <= 0){
+            size = 20;
+        }
+        //分页对象
+        Pageable pageable = PageRequest.of(page,size);
+        //分页查询
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        QueryResult<CmsPage> queryResult = new QueryResult<>();
+        queryResult.setList(all.getContent());
+        queryResult.setTotal(all.getTotalElements());
+        //返回结果
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
+        return queryResponseResult;
+    }
+}
