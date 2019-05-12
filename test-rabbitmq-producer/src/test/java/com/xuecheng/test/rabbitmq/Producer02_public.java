b55c021f8729ee7fd29289d7f04814d6c7cc6c55
vonco <vonco@itheima.com>
2019-05-12 16:14:21 +0800

    day05:rabbitmq

---
+++ test-rabbitmq-producer/src/test/java/com/xuecheng/test/rabbitmq/Producer02_public.java
@@ -0,0 +1,94 @@
+package com.xuecheng.test.rabbitmq;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer02_public {
+    private static final String QUEUE_INFORM_EMAIL = "queue_inform_email";
+    private static final String QUEUE_INFORM_SMS = "queue_inform_sms";
+    private static final String EXCHANGE_FANOUT_INFORM = "exchange_fanout_inform";
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("127.0.0.1");
+            factory.setPort(5672);
+            factory.setUsername("guest");
+            factory.setPassword("guest");
+            factory.setVirtualHost("/");//rabbitmq默认虚拟机名称为“/”，虚拟机相当于一个独立的mq服务
+
+            //创建与RabbitMQ服务的TCP连接
+            connection = factory.newConnection();
+            //创建与Exchange的通道，每个连接可以创建多个通道，每个通道代表一个会话任务
+            channel = connection.createChannel();
+            /**
+             * 声明队列，如果Rabbit中没有此队列将自动创建
+             * param1:队列名称
+             * param2:是否持久化
+             * param3:队列是否独占此连接
+             * param4:队列不再使用时是否自动删除此队列
+             * param5:队列参数
+             */
+            channel.queueDeclare(QUEUE_INFORM_EMAIL,true,false,false,null);
+            channel.queueDeclare(QUEUE_INFORM_SMS,true,false,false,null);
+            /**
+             * 参数明细
+             * 1、交换机名称
+             * 2、交换机类型，fanout、topic、direct、headers
+             */
+            channel.exchangeDeclare(EXCHANGE_FANOUT_INFORM, BuiltinExchangeType.FANOUT);
+            /**
+             * 参数明细
+             * 1、队列名称
+             * 2、交换机名称
+             * 3、路由key
+             */
+            channel.queueBind(QUEUE_INFORM_EMAIL,EXCHANGE_FANOUT_INFORM,"");
+            channel.queueBind(QUEUE_INFORM_SMS,EXCHANGE_FANOUT_INFORM,"");
+            for (int i = 0; i<10 ; i++) {
+                String message = "inform to user"+i;
+                /**
+                 * 消息发布方法
+                 * param1：Exchange的名称，如果没有指定，则使用Default Exchange
+                 * param2:routingKey,消息的路由Key，是用于Exchange（交换机）将消息转发到指定的消息队列
+                 * param3:消息包含的属性
+                 * param4：消息体
+                 */
+                /**
+                 * 这里没有指定交换机，消息将发送给默认交换机，每个队列也会绑定那个默认的交换机，但是不能显
+                 示绑定或解除绑定
+                 * 默认的交换机，routingKey等于队列名称
+                 */
+                channel.basicPublish(EXCHANGE_FANOUT_INFORM, "", null, message.getBytes());
+                System.out.println("Send Message is:'" + message + "'");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            if(channel!=null){
+                try {
+                    channel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (TimeoutException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(connection!=null){
+                try {
+                    connection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
